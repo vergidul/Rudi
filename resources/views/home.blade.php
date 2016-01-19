3acfa99087ce68f93cb13e7d55655102f29275d3
vergidul <stefanocrognale@hotmail.it>
2016-01-19 12:48:10 +0100

    facebook

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,7 +10,7 @@ Webcam
 
 <p> Ecco le webcam </p>
 
-<div class="fb-like" data-href="https://www.facebook.com/diabetesmarathonforli/" data-layout="standard" data-action="like" data-show-faces="false" data-share="true"></div>
+<div class="fb-like" data-href="https://www.facebook.com/SURF-TRIPit-1661144574114490/?pnref=lhc" data-layout="standard" data-action="like" data-show-faces="true" data-share="true"></div>
 
 
 
