da46f874ffdfc2327a6799f135e87b298bdd9a17
vergidul <stefanocrognale@hotmail.it>
2016-01-19 11:45:00 +0100

    progetto due

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -7,7 +7,9 @@
     <meta name="description" content="Example Laravel Application">
     <meta name="author" content="Leonardo Dal Zovo">
 	<meta name="csrf-token" content="{{ csrf_token() }}" />
+	
 	<title>@yield('title')</title>
+	
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
 	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
@@ -16,146 +18,105 @@
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
 	<style type="text/css">
-		/*
-		 * Base structure
-		 */
-		
-		/* Move down content because we have a fixed navbar that is 50px tall */
-		body {
-		  padding-top: 50px;
-		}
-		
-		/*
-		 * Global add-ons
-		 */
-		.sub-header {
-		  padding-bottom: 10px;
-		  border-bottom: 1px solid #eee;
-		}
-		
-		/*
-		 * Top navigation
-		 * Hide default border to remove 1px line.
-		 */
-		.navbar-fixed-top {
-		  border: 0;
-		}
-		
-		/*
-		 * Sidebar
-		 */
-		
-		/* Hide for mobile, show later */
-		.sidebar {
-		  display: none;
-		}
-		@media (min-width: 768px) {
-		  .sidebar {
-		    position: fixed;
-		    top: 51px;
-		    bottom: 0;
-		    left: 0;
-		    z-index: 1000;
-		    display: block;
-		    padding: 20px;
-		    overflow-x: hidden;
-		    overflow-y: auto; /* Scrollable contents if viewport is shorter than content. */
-		    background-color: #f5f5f5;
-		    border-right: 1px solid #eee;
-		  }
-		}
-		
-		/* Sidebar navigation */
-		.nav-sidebar {
-		  margin-right: -21px; /* 20px padding + 1px border */
-		  margin-bottom: 20px;
-		  margin-left: -20px;
-		}
-		.nav-sidebar > li > a {
-		  padding-right: 20px;
-		  padding-left: 20px;
-		}
-		.nav-sidebar > .active > a,
-		.nav-sidebar > .active > a:hover,
-		.nav-sidebar > .active > a:focus {
-		  color: #fff;
-		  background-color: #428bca;
-		}
-		
-		
-		/*
-		 * Main content
-		 */
-		.main {
-		  padding: 20px;
-		}
-		@media (min-width: 768px) {
-		  .main {
-		    padding-right: 40px;
-		    padding-left: 40px;
-		  }
-		}
-		.main .page-header {
-		  margin-top: 0;
-		}
-		
-		
-		/*
-		 * Placeholder dashboard ideas
-		 */
-		.placeholders {
-		  margin-bottom: 30px;
-		  text-align: center;
-		}
-		.placeholders h4 {
-		  margin-bottom: 0;
-		}
-		.placeholder {
-		  margin-bottom: 20px;
-		}
-		.placeholder img {
-		  display: inline-block;
-		  border-radius: 50%;
-		}
-	</style>
+
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  
+  <style>
+    /* Remove the navbar's default margin-bottom and rounded borders */ 
+    .navbar {
+      margin-bottom: 0;
+      border-radius: 0;
+    }
+    
+    /* Set height of the grid so .sidenav can be 100% (adjust as needed) */
+    .row.content {height: 450px}
+    
+    /* Set gray background color and 100% height */
+    .sidenav {
+      padding-top: 20px;
+      background-color: #f1f1f1;
+      height: 100%;
+    }
+    
+    /* Set black background color, white text and some padding */
+    footer {
+      background-color: #555;
+      color: white;
+      padding: 15px;
+    }
+    
+    /* On small screens, set height to 'auto' for sidenav and grid */
+    @media screen and (max-width: 767px) {
+      .sidenav {
+        height: auto;
+        padding: 15px;
+      }
+      .row.content {height:auto;} 
+    }
+  </style>
 </head>
+<body>
+
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>                        
+      </button>
+      <a class="navbar-brand" href="#">Baliwebcam</a>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="#">Home</a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Projects</a></li>
+        <li><a href="#">Contact</a></li>
+      </ul>
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
+  
+
+<div class="container-fluid text-center">    
+  <div class="row content">
+    <div class="col-sm-2 sidenav">
+      <p><a href="#">Link</a></p>
+      <p><a href="#">Link</a></p>
+      <p><a href="#">Link</a></p>
+    </div>
+    
+ 
+    <div class="col-sm-8 text-left"> 
+      @yield('content') 
+    </div>
+    
+    <div class="col-sm-2 sidenav">
+      <div class="well">
+        <p>ADS</p>
+      </div>
+      <div class="well">
+        <p>ADS</p>
+      </div>
+    </div>
+  </div>
+</div>
+
+<footer class="container-fluid text-center">
+  <p>Footer Text</p>
+</footer>
+
+</body>
+</html>
 
 <body>
 	
-    <!-- Fixed navbar -->
-    <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-      <div class="container">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <!--  <a class="navbar-brand" href="contoller auth">Login</a> -->
-        </div>
-        <div class="navbar-collapse collapse">
-          <ul class="nav navbar-nav">
-            <li class="active"><a href="#">Home</a></li>
-            <li><a href="#about">About</a></li>
-            <li><a href="#contact">Contact</a></li>
-            <li class="dropdown">
-              <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
-              <ul class="dropdown-menu">
-                <li><a href="#">Action</a></li>
-                <li><a href="#">Another action</a></li>
-                <li><a href="#">Something else here</a></li>
-                <li class="divider"></li>
-                <li class="dropdown-header">Nav header</li>
-                <li><a href="#">Separated link</a></li>
-                <li><a href="#">One more separated link</a></li>
-              </ul>
-            </li>
-          </ul>
-        </div><!--/.nav-collapse -->
-        </div>
-        </div>
-      <div class="container"> @yield('content') </div> 
+   
+      <div class="container"> </div> 
       
 </body>
 
--- storage/a740a0441caad70dbce48cc9a1c13b0091ad5fd1.php
+++ storage/a740a0441caad70dbce48cc9a1c13b0091ad5fd1.php
@@ -7,55 +7,9 @@ Webcam
 
 <?php $__env->startSection('content'); ?> 
 
-<h1>Immagini</h1>
+<p> Ecco le webcam </p>
 
-<style>
-.carousel {
-  width:auto;
-}
-</style>
 
-<div id="myCarousel" class="carousel slide" data-ride="carousel">
-  <!-- Indicators -->
-  <ol class="carousel-indicators">
-    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-    <li data-target="#myCarousel" data-slide-to="1"></li>
-    <li data-target="#myCarousel" data-slide-to="2"></li>
-    
-  </ol>
-
-  <!-- Wrapper for slides -->
-  <div class="carousel-inner" role="listbox">
-    <div class="item active">
-      <img src="1.jpg" alt="Chania">
-    </div>
-
-    <div class="item">
-      <img src="2.jpg" alt="Chania">
-    </div>
-
-    <div class="item">
-      <img src="3.jpg" alt="Immagine mancante :)">
-    </div>
-
-    
-  </div>
-
-  <!-- Left and right controls -->
-  <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
-    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-    <span class="sr-only">Previous</span>
-  </a>
-  <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
-    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-    <span class="sr-only">Next</span>
-  </a>
-</div>
-</br>
-<div class="well">
-<h1>Presentazione</h1>
-<p>Io sono</p>
-</div>
 
 <?php $__env->stopSection(); ?>
 
